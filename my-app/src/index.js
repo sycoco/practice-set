4ffd788d305d365474a102c4f373ffc92907645f
ying sha <153262770@qq.com>
2018-07-06 16:11:14 +0800

    评论功能

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,93 +1,9 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
+import CommentApp from './CommentApp'
 import './index.css'
 
-class Header extends Component {
-    render () {
-        return (
-            <div>
-                <Title/>
-                <LikeButton wordings={{likedText: '已赞1', unlikedText: '赞2'}}
-                            onClick={() => console.log('Click on like button!')}/>
-            </div>
-        )
-    }
-}
-class Title extends Component {
-    handleClickOnTitle (word,e) {
-        console.log(this,word);
-    }
-    render () {
-        return (
-            <h1 onClick={this.handleClickOnTitle.bind(this,'hello')}>React title 小书</h1>
-        )
-    }
-}
-class Main extends Component {
-    render () {
-        return (
-            <p>this is main</p>
-        )
-    }
-}
-class Footer extends Component {
-    render () {
-        return (
-            <p>this is Footer</p>
-        )
-    }
-}
-class Wrapper extends Component {
-    render(){
-        return(
-            <div>
-                <Header/>
-                <Main/>
-                <Footer/>
-            </div>
-        )
-    }
-}
-class LikeButton extends Component {
-    constructor () {
-        super();
-        this.state = { isLiked: false , isSelected: false}
-    }
-
-    handleClickOnLikeButton () {
-        this.setState({
-            isLiked: !this.state.isLiked,
-            isSelected: !this.state.isSelected
-        });
-        if (this.props.onClick) {
-            this.props.onClick();
-        }
-    }
-
-    render () {
-        const users = [
-            { username: 'Jerry', age: 21, gender: 'male' },
-            { username: 'Tomy', age: 22, gender: 'male123' },
-            { username: 'Lily', age: 19, gender: 'female' },
-            { username: 'Lucy', age: 20, gender: 'female' }
-        ];
-        return (
-            <div>
-                {users.map((user) => {
-                    return (
-                        <div>
-                            <div>姓名：{user.username}</div>
-                            <div>年龄：{user.age}</div>
-                            <div>性别：{user.gender}</div>
-                            <hr />
-                        </div>
-                    )
-                })}
-            </div>
-        )
-    }
-}
 ReactDOM.render(
-    <LikeButton />,
+    <CommentApp />,
     document.getElementById('root')
-);
+)
